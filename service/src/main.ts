6ea93de1d8cd329af6e8fc948a6dd0f984b0ea83
Karsten Panier <karsten.panier@signal-iduna.de>
2017-12-30 21:42:18 +0100

    Dockerfile for service
    - Reorder docker commands for smaller build
    - Lookup db connection string from env

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,7 +1,11 @@
 import { APIServer } from './APIServer';
 import MongoClient = require('mongodb');
-
-MongoClient.connect('mongodb://dbuser:notes-enc@localhost:27037/notes-db', function (err, mongodb) {
+let url = 'mongodb://dbuser:notes-enc@localhost:27037/notes-db';
+if (process.env.mongourl) {
+    url = process.env.mongourl;
+    console.log('Use env for mongodb')
+}
+MongoClient.connect(url, function (err, mongodb) {
     if (err) {
         console.log(err);
     }
