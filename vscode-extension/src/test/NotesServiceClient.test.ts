7b3194c22e09ded0628536cfb2e158f4a628db88
Karsten Panier <karsten.panier@signal-iduna.de>
2018-02-19 08:25:45 +0100

    Travis CI
    - Run vscode tests
    - Remove dummy test

--- vscode-extension/src/test/NotesServiceClient.test.ts
+++ vscode-extension/src/test/NotesServiceClient.test.ts
@@ -6,24 +6,19 @@ import { NotesServiceClient } from '../core/NotesServiceClient';
 import { HttpClient, HttpClientResponse } from 'typed-rest-client/HttpClient';
 import { IncomingMessage } from 'http';
 
-suite("NoteService client Tests", () => {
+suite("NoteService client tests", () => {
 
-    test("Test header", () => {
-        // given
+    test("Test http request header", () => {
         let service = new NotesServiceClient();
         service.token = 'Test Token'
 
-        // when
         let header = service.createHeaders();
-
-        // then
 
         assert.equal(header['Content-Type'], 'application/json');
         assert.equal(header['x-access-token'], service.token);
     });
 
     test("Test get all notes", async () => {
-        // given
         let service = new NotesServiceClient();
         service.token = 'Test Token'
         let httpClMock: TypeMoq.IMock<HttpClient> = TypeMoq.Mock.ofType(HttpClient);
@@ -34,11 +29,8 @@ suite("NoteService client Tests", () => {
         httpClMock.setup(mock => mock.get(TypeMoq.It.isAnyString(), service.createHeaders())).returns(() => Promise.resolve(respMock.object));
         respMock.setup(mock => mock.message).returns(() => msgMock.object);
         respMock.setup(mock => mock.readBody()).returns(() => Promise.resolve('[ { "name": "ToDo", "content": "Some content"}]'));
-
-        // when
         let notes = await service.getNoteList().then(n => { return n });
 
-        // then
         assert.equal(1, notes.length);
         assert.equal('ToDo', notes[0].name);
         assert.equal(true, notes[0].remote);
